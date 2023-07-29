micahcowan/bobbin

commit 71e989e124dda4a04203942332f16235d5382f8e

Update tty with video writes.

src/bobbin-internal.h
@@ -257,7 +257,7 @@ struct IfaceDesc {
     void (*prestep)(void);
     void (*step)(void);
     int  (*peek)(word loc);
-    int  (*poke)(word loc, byte val);
+    bool (*poke)(word loc, byte val);
     void (*frame)(bool flash);
     void (*enter_dbg)(FILE **inf, FILE **outf);
     void (*exit_dbg)(void);
@@ -285,6 +285,7 @@ extern void rh_prestep(void); // The only hooks allowed to examine and
                               // anything else.
 extern void rh_step(void);
 //extern int  rh_peek_sneaky(word loc);
+extern bool rh_poke(word loc, byte val);
 extern int  rh_peek(word loc);
 
 /********** TRACE **********/

src/hook.c
@@ -50,3 +50,13 @@ int rh_peek(word loc)
     // is called for.
     assert(pc == current_pc());
 }
+
+bool rh_poke(word loc, byte val)
+{
+    word pc = current_pc();
+    return iface_poke(loc, val);
+    // For "user" hooks, we should emit a warning, and reset PC after
+    // any hook that set it. But for our internal stuff, stronger action
+    // is called for.
+    assert(pc == current_pc());
+}

src/interfaces/simple.c
@@ -434,11 +434,6 @@ static int iface_simple_peek(word loc)
     return -1;
 }
 
-static int iface_simple_poke(word loc, byte val)
-{
-    return -1;
-}
-
 static void iface_simple_enter_dbg(FILE **in, FILE **out)
 {
     if (!interactive) {
@@ -479,7 +474,6 @@ IfaceDesc simpleInterface = {
     .prestep = iface_simple_prestep,
     .step = iface_simple_step,
     .peek = iface_simple_peek,
-    .poke = iface_simple_poke,
     .enter_dbg = iface_simple_enter_dbg,
     .exit_dbg = iface_simple_exit_dbg,
 };

src/interfaces/tty.c
@@ -20,6 +20,19 @@ static word get_line_base(byte page, byte y)
     return WORD(lo, hi);
 }
 
+static byte get_line_for_addr(word loc)
+{
+    loc &= 0x03F8; // discard uninteresting high bits
+    // lowest bit (0) of the line # is loc & 0x0080
+    // bits 1 and 2 are loc & 0x0300
+    byte y = (loc & 0x0380) >> 7;
+    // The remaining high bytes are determined by ranges
+    byte lo = loc & 0x7F;
+    y |= lo < 0x28? 0
+        : (lo < 0x50? 0x8 : 0x10);
+    return y;
+}
+
 static void repaint_flash(bool flash)
 {
     saved_flash = flash;
@@ -89,7 +102,20 @@ static void if_tty_start(void)
     refresh_video(false);
 }
 
-static void if_tty_step(void) {
+static bool if_tty_poke(word loc, byte val)
+{
+    byte x = loc & 0x7F;
+    if (loc >= 0x400 && loc < 0x800 && x < 120) {
+        x %= 40;
+        byte y = get_line_for_addr(loc);
+
+        int c = util_todisplay(val);
+        bool f = util_isflashing(val) && saved_flash;
+        if (f) wattron(win, A_REVERSE);
+        mvwaddch(win, y, x, c);
+        if (f) wattroff(win, A_REVERSE);
+    }
+    return false;
 }
 
 static void if_tty_frame(bool flash)
@@ -108,6 +134,6 @@ static void if_tty_frame(bool flash)
 
 IfaceDesc ttyInterface = {
     .start = if_tty_start,
-    .step = if_tty_step,
+    .poke  = if_tty_poke,
     .frame = if_tty_frame,
 };

src/mem.c
@@ -193,8 +193,8 @@ byte peek_sneaky(word loc)
 
 void poke(word loc, byte val)
 {
-    // suppress_real_write = rh_poke(loc, val)...
-    poke_sneaky(loc, val);
+    if (!rh_poke(loc, val))
+        poke_sneaky(loc, val);
 }
 
 void poke_sneaky(word loc, byte val)

commit 6044a04d0d23af4fd00fc39ec85df2799148c460

--simple broke in 71e989e: fixed.

src/bobbin-internal.h
@@ -268,7 +268,7 @@ extern void interfaces_start(void);
 
 extern void iface_prestep(void);
 extern void iface_step(void);
-extern int  iface_poke(word loc, byte val);
+extern bool iface_poke(word loc, byte val);
                                   // if -1 is NOT returned, suppress
                                   // actual memory write (because iface
                                   // intercepted the write)

src/interfaces/iface.c
@@ -83,9 +83,9 @@ int iface_peek(word loc)
     return iii->peek? iii->peek(loc) : -1;
 }
 
-int iface_poke(word loc, byte val)
+bool iface_poke(word loc, byte val)
 {
-    return iii->poke? iii->poke(loc, val) : -1;
+    return iii->poke? iii->poke(loc, val) : false;
 }
 
 void iface_frame(bool flash)