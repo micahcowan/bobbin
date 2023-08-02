5a5dd092fc0b9345c07e7facae2f13a48bf2d794
Micah Cowan <micah@addictivecode.org>
2023-08-02 15:01:40 -0700

    tty: added a messages window, with test messages.

--- src/interfaces/tty.c
+++ src/interfaces/tty.c
@@ -8,7 +8,11 @@ static const word text_size = 0x400;
 static byte text_page = 0x4;
 
 static WINDOW *win;
+static WINDOW *msgwin;
 static bool saved_flash;
+static bool refresh_overlay = false;
+static const unsigned long overlay_wait = 120; // 2 seconds
+static unsigned long overlay_timer = 0;
 static byte typed_char = '\0';
 
 static void tty_atexit(void)
@@ -37,6 +41,14 @@ static byte get_line_for_addr(word loc)
     return y;
 }
 
+static void do_overlay(void)
+{
+    int y, x;
+    getyx(msgwin, y, x);
+    if (x == 0 && y == 0) return;
+    copywin(msgwin, win, 0, 0, 23 - y + (x? 0: 1), 0, 23, 39, false);
+}
+
 static void repaint_flash(bool flash)
 {
     saved_flash = flash;
@@ -59,7 +71,6 @@ static void refresh_video(bool flash)
 {
     saved_flash = flash;
     wattrset(win, A_NORMAL);
-    bool last_was_flash = false;
     for (int y=0; y != 24; ++y) {
         word base = get_line_base(text_page, y);
         wmove(win, y, 0);
@@ -67,18 +78,27 @@ static void refresh_video(bool flash)
             byte c = peek_sneaky(base + x);
             byte cd = util_todisplay(c);
             bool cfl = util_isreversed(c, flash);
-            if (cfl != last_was_flash) {
-                last_was_flash = cfl;
-                if (cfl) {
-                    wattron(win, A_REVERSE);
-                } else {
-                    wattroff(win, A_REVERSE);
-                }
-            }
-            waddch(win, cd);
+            waddch(win, cd | (cfl? A_REVERSE: 0));
         }
     }
-    wattrset(win, A_NORMAL);
+
+    do_overlay();
+}
+
+static void clear_overlay(void)
+{
+    wattron(msgwin, COLOR_PAIR(0));
+    wclear(msgwin);
+    wmove(msgwin, 0, 0);
+    wrefresh(msgwin);
+    wattron(msgwin, COLOR_PAIR(1));
+    refresh_video(saved_flash);
+}
+
+static void do_overlay_timer(void)
+{
+    if (!overlay_timer) return;
+    if (--overlay_timer == 0) clear_overlay();
 }
 
 static void draw_border(void)
@@ -97,6 +117,7 @@ static void if_tty_start(void)
     }
     // Init curses
     initscr();
+    start_color();
     raw(); //cbreak();
     noecho();
     nonl();
@@ -111,6 +132,16 @@ static void if_tty_start(void)
     nodelay(win, 1);
 
     draw_border();
+
+    // Messaging window test
+    msgwin = newwin(24, 40, 0, 0);
+    init_pair(1, COLOR_BLACK, COLOR_CYAN);
+    wattron(msgwin, COLOR_PAIR(1));
+    wprintw(msgwin, "Here is a line of text.\n");
+    wprintw(msgwin, "Here is another!\n");
+    wprintw(msgwin, "...and yet another.\n");
+    wrefresh(msgwin);
+    overlay_timer = overlay_wait;
 
     // Draw current video memory (garbage)
     refresh_video(false);
@@ -158,10 +189,9 @@ static bool if_tty_poke(word loc, byte val)
         byte y = get_line_for_addr(loc);
 
         int c = util_todisplay(val);
-        bool f = util_isreversed(val, saved_flash);
-        if (f) wattron(win, A_REVERSE);
+        if (util_isreversed(val, saved_flash)) c |= A_REVERSE;
         mvwaddch(win, y, x, c);
-        if (f) wattroff(win, A_REVERSE);
+        refresh_overlay = true;
     } else {
         if_tty_peek_or_poke(loc);
     }
@@ -171,8 +201,14 @@ static bool if_tty_poke(word loc, byte val)
 
 static void if_tty_frame(bool flash)
 {
+    do_overlay_timer();
     if (flash != saved_flash) {
         repaint_flash(flash);
+        refresh_overlay = true;
+    }
+    if (refresh_overlay) {
+        refresh_overlay = false;
+        do_overlay();
     }
     wrefresh(win);
 
